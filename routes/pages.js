41f1c6d79c3c886596683d0c0792af0ee55587cc
skipptekk <skipptekk@gmail.com>
2021-01-01 09:30:53 -0700

    callback switch/register update

--- routes/pages.js
+++ routes/pages.js
@@ -28,7 +28,14 @@ memoryLeft = os.totalmem();
 memoryFree = memoryLeft*0.000001;
 memoryTotal = os.freemem();
 memorytotal = memoryTotal*0.000001;
-
+//login shit
+var session = require('express-session')
+app.use(session({
+  secret: process.env.loginsecret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 3600 }
+}));
 app.get('/',(req, res) =>{
   res.render('index',{title: 'Home Page'})
 });
@@ -98,7 +105,7 @@ app.get('/ships', function (req, res) {
   const project = ESIIL({
     clientID: process.env.SSOClient,
     clientSecret: process.env.SSOSecret,
-    callbackURL: 'http://localhost:3000/callback',
+    callbackURL: process.env.SSOcallback,
     userAgent: 'Biscuits Industrial IGN I Like Biscuits',
     state: 'MyState',
     scopes: ['publicData', 'esi-location.read_location.v1', 'esi-location.read_ship_type.v1', 'esi-mail.read_mail.v1', 'esi-skills.read_skills.v1', 'esi-skills.read_skillqueue.v1', 'esi-wallet.read_character_wallet.v1', 'esi-clones.read_clones.v1', 'esi-killmails.read_killmails.v1', 'esi-assets.read_assets.v1', 'esi-characters.read_medals.v1', 'esi-characters.read_standings.v1', 'esi-industry.read_character_jobs.v1', 'esi-characters.read_blueprints.v1', 'esi-location.read_online.v1', 'esi-clones.read_implants.v1', 'esi-characters.read_fatigue.v1', 'esi-industry.read_corporation_jobs.v1', 'esi-industry.read_character_mining.v1', 'esi-characters.read_titles.v1']
